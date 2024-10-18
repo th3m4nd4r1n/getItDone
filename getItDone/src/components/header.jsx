deb7a37918cca5bf95b2923f11064e621aea24fb
th3m4nd4r1n <mdua_be23@thapar.edu>
2024-10-18 23:27:24 +0530

    Created the header for now. Only frontend work done yet

---
+++ getItDone/src/components/header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <>
+         <div className="mainContainer">
+            <div className="blur"></div>
+            
+            <div className="flex">
+                
+            <p className="head">getItDone</p>
+            </div>
+            <div className="image">
+
+           <img src="../../public/logo_head.png" alt="logo" />
+</div>
+
+
+            
+            </div>        
+        </>
+    )
+}
+export default Header;
